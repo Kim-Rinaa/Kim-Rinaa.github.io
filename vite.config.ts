f19b1733bb079784481870d4cfc41f433f8f7798
Kim-Rinaa <hine1017@gmail.com>
2025-05-28 15:11:00 +0900

    naver sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import { componentTagger } from "lovable-tagger"
+import sitemap from 'vite-plugin-sitemap' 
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' &&
-      componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+    sitemap({ hostname: 'https://www.ihubglobal.co.kr' }) 
+  ],
   base: '/',
-}));
+
+})
